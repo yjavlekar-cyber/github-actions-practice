6ccadc8ca861024a5e2dd0cb54789cb42feed912
Yogesh Jawlekar <yjawlekar@gmel.com>
2026-06-22 17:41:38 +0000

    healthcheck run

--- .github/workflows/healthcheck.sh
+++ .github/workflows/healthcheck.sh
@@ -18,4 +18,4 @@ echo "---Disk Usage---"
 df -h / | awk 'NR==2 {print "Total: "$2 "  Used: "$3 "  Free: "$4 "  Use%: "$5}'
 
 echo ""
-echo "===== End of Report ====="|tee -a /mnt/c/Users/yogesh\ jawlekar/projects/system-health-monitor/health.log
+echo "===== End of Report ====="
